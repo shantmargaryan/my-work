0a0826d892cd709a07b477ced9783f36c1182315
Shant <shant212205@gmail.com>
2023-12-20 07:29:44 -0800

    update

--- js/main.js
+++ js/main.js
@@ -82,9 +82,30 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-        
+
         992: {
             slidesPerView: 3,
         }
     }
 });
+
+function getScrollAnimation() {
+    window.addEventListener("scroll", getScrollAnimation);
+    const scrollAnim = document.querySelectorAll(".scroll-anim");
+    if (scrollAnim.length > 0) {
+        scrollAnim.forEach(animItem => {
+            const { top, height } = animItem.getBoundingClientRect();
+            const offset = window.scrollY + top
+            const scrollAnimPoint = window.innerHeight - height / 3
+            if (offset < window.scrollY + scrollAnimPoint && offset + height > window.scrollY) {
+                animItem.classList.add("scroll-anim_active")
+            } else {
+                if (!animItem.classList.contains("scroll-anim_once")) {
+                    animItem.classList.remove("scroll-anim_active")
+                }
+            }
+        });
+    }
+};
+
+setTimeout(getScrollAnimation, 300);
